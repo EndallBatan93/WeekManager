7c0ff6b570008227afcc1d0a74bac1a68c5120ef
Manuel Hinke <manuel.hinke@uni-ulm.de>
2018-04-02 21:27:44 +0200

    started weeklayout

--- app/src/main/java/com/example/endallbatan/weekmanager/MainScreenActivity.kt
+++ app/src/main/java/com/example/endallbatan/weekmanager/MainScreenActivity.kt
@@ -1,10 +1,12 @@
 package com.example.endallbatan.weekmanager
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.View
 import android.widget.Button
+import android.widget.ListView
 import android.widget.TextView
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DatabaseReference
@@ -17,15 +19,19 @@ class MainScreenActivity : AppCompatActivity() {
     override fun onStart() {
         super.onStart()
         val mUser = mFirebaseAuth!!.currentUser
-        val userReference = mDatabaseReference!!.child(mUser!!.uid)
-        val email = findViewById<View>(R.id.email) as TextView
-        email.text = mUser.email
     }
     override fun onCreate(savedInstanceState: Bundle?) {
         setContentView(R.layout.main_screen_activity)
         super.onCreate(savedInstanceState)
+        actionBarSetup()
         initialise()
     }
+
+    private fun actionBarSetup() {
+        title = "Your Week"
+    }
+
+
 
     private fun initialise() {
         mDatabase = FirebaseDatabase.getInstance()
